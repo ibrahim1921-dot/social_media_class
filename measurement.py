521b27b5223b46ca832fffa3f7d7bbc4ad8fc2e1
ibrahim1921-dot <abdulsobur1921@gmail.com>
2025-06-13 12:58:08 +0100

    Update measurement.py

--- measurement.py
+++ measurement.py
@@ -7,18 +7,18 @@ def main():
     inches=input("Enter the number of inches: ")
     return feet_and_inches_to_centimeters(float(feet), float(inches))
 
-#function to convert feet and inches to centimeters.
 
-def feet_and_inches_to_centimeters(feet, inches):
      
+#function to convert feet and inches to centimeters.
+def feet_and_inches_to_centimeters(feet, inches):
     inch_per_foot= 12
     centimeters_per_inch = 2.54
     total_inches = feet * inch_per_foot + inches
     centimeter= total_inches * centimeters_per_inch
     return centimeter
 
-# Main function to execute the program
 
+# Main function to execute the program
 if __name__ == "__main__":
     centimeters = main()
     print(f"The measurement in centimeters is: {centimeters:.2f} cm")
